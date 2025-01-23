4cbd4b776b2c0f8ad7afcac1691779e2ec749d94
Meher Howji <hmeherranjan@gmail.com>
2025-01-23 14:57:08 +0530

    package update + sass migrator

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,32 +1,32 @@
-'use client';
+'use client'
 
-import { useState, useEffect, useRef } from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
-import DesktopNav from '@/components/desktop-nav';
-import useDimensions from '@/hooks/useDimensions';
-import simplifySvgPath from '@/hooks/simplifySvgPath';
+import { useState, useEffect, useRef } from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import DesktopNav from '@/components/desktop-nav'
+import useDimensions from '@/hooks/useDimensions'
+import simplifySvgPath from '@/hooks/simplifySvgPath'
 
-import css from '@/styles/component-css/footer.module.scss';
+import css from '@/styles/component-css/footer.module.scss'
 
 interface FooterProps {
-  showNewsletter?: boolean;
+  showNewsletter?: boolean
 }
 
 interface SocialBarProps {
-  size: number;
+  size: number
 }
 
 interface SvgWaveProps {
   parentAttr: {
-    width: number;
-    height: number;
-  };
+    width: number
+    height: number
+  }
 }
 
 const Footer: React.FC<FooterProps> = ({ showNewsletter = true }) => {
-  const footerRef = useRef<HTMLDivElement>(null);
-  const parentAttr = useDimensions(footerRef);
+  const footerRef = useRef<HTMLDivElement>(null)
+  const parentAttr = useDimensions(footerRef)
 
   return (
     <>
@@ -35,25 +35,15 @@ const Footer: React.FC<FooterProps> = ({ showNewsletter = true }) => {
         <div className={css.container}>
           <div className={css.footerSubTitle}>
             <div className={css.flex}>
-              <Image
-                src="./assets/brand/logo.svg"
-                alt="Meher+Howji logo"
-                width="36"
-                height="36"
-                priority={true}
-              />
+              <Image src="./assets/brand/logo.svg" alt="Meher+Howji logo" width="36" height="36" priority={true} />
               <SocialBar size={16} />
             </div>
             <div>
               <p className={css.siteTech}>
-                <span>
-                  Built with NextJS, Supabase & using CSS Grids & Flexbox. Learn{' '}
-                </span>
+                <span>Built with NextJS, Supabase & vanilla CSS grids+flexbox. Learn </span>
                 <Link href="/how-i-built-my-website">how.</Link>
               </p>
-              <p className={css.copyright}>
-                Meher Howji © {new Date().getFullYear()}
-              </p>
+              <p className={css.copyright}>Meher Howji © {new Date().getFullYear()}</p>
               <p>
                 <Link href="/terms-and-conditions" passHref>
                   Terms of Service
@@ -67,16 +57,13 @@ const Footer: React.FC<FooterProps> = ({ showNewsletter = true }) => {
           </div>
 
           <div className={css.footerNav}>
-            <DesktopNav
-              onFooter={true}
-              onMobileNavToggle={function (): void {}}
-            />
+            <DesktopNav onFooter={true} onMobileNavToggle={function (): void {}} />
           </div>
         </div>
       </footer>
     </>
-  );
-};
+  )
+}
 
 const SocialBar: React.FC<SocialBarProps> = ({ size }) => {
   return (
@@ -105,63 +92,63 @@ const SocialBar: React.FC<SocialBarProps> = ({ size }) => {
         </svg>
       </Link>
     </div>
-  );
-};
+  )
+}
 
 const SvgWave: React.FC<SvgWaveProps> = ({ parentAttr }) => {
-  const [perlinLine, setPerlinLine] = useState<string>('');
+  const [perlinLine, setPerlinLine] = useState<string>('')
 
   useEffect(() => {
-    setPerlinLine(get1DPerlinNoise(parentAttr.height));
+    setPerlinLine(get1DPerlinNoise(parentAttr.height))
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [])
 
   function rand(): number {
-    const M = 4294967296;
-    const A = 1664525;
-    const C = 1;
-    let Z = Math.floor(Math.random() * M);
-    Z = (A * Z + C) % M;
-    return Z / M - 0.5;
+    const M = 4294967296
+    const A = 1664525
+    const C = 1
+    let Z = Math.floor(Math.random() * M)
+    Z = (A * Z + C) % M
+    return Z / M - 0.5
   }
 
   function interpolate(pa: number, pb: number, px: number): number {
-    const ft = px * Math.PI;
-    const f = (1 - Math.cos(ft)) * 0.5;
-    return pa * (1 - f) + pb * f;
+    const ft = px * Math.PI
+    const f = (1 - Math.cos(ft)) * 0.5
+    return pa * (1 - f) + pb * f
   }
 
   function get1DPerlinNoise(parentHeight: number): string {
-    let x = 0;
-    const w = window.innerWidth;
-    const h = 100;
-    let y = h / 2;
-    const amp = 100; // amplitude
-    const wl = 250; // wavelength
-    const fq = 1 / wl; // frequency
-    let a = rand();
-    let b = rand();
-    const xyz: [number, number][] = [];
+    let x = 0
+    const w = window.innerWidth
+    const h = 100
+    let y = h / 2
+    const amp = 100 // amplitude
+    const wl = 250 // wavelength
+    const fq = 1 / wl // frequency
+    let a = rand()
+    let b = rand()
+    const xyz: [number, number][] = []
 
     while (x < w) {
       if (x % wl === 0) {
-        a = b;
-        b = rand();
-        y = h / 2 + a * amp;
+        a = b
+        b = rand()
+        y = h / 2 + a * amp
       } else {
-        y = h / 2 + interpolate(a, b, (x % wl) / wl) * amp;
+        y = h / 2 + interpolate(a, b, (x % wl) / wl) * amp
       }
-      xyz.push([x, y]);
-      x += 1;
+      xyz.push([x, y])
+      x += 1
     }
 
     let path = simplifySvgPath(xyz, {
       tolerance: 2.5,
       precision: 1,
-    });
-    const reset = path.substring(path.indexOf('c')) + 1;
-    path = 'M0,100' + reset + `L${w},${parentHeight}L0,${parentHeight}L0,100Z`;
-    return path;
+    })
+    const reset = path.substring(path.indexOf('c')) + 1
+    path = 'M0,100' + reset + `L${w},${parentHeight}L0,${parentHeight}L0,100Z`
+    return path
   }
 
   return (
@@ -170,8 +157,7 @@ const SvgWave: React.FC<SvgWaveProps> = ({ parentAttr }) => {
       style={{
         width: `${parentAttr.width}px`,
         height: `${parentAttr.height}px`,
-      }}
-    >
+      }}>
       <svg xmlns="http://www.w3.org/2000/svg">
         <defs>
           <clipPath id="wave">
@@ -180,7 +166,7 @@ const SvgWave: React.FC<SvgWaveProps> = ({ parentAttr }) => {
         </defs>
       </svg>
     </div>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
